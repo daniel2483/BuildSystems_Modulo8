f411f0b20cec6667aababce8f7438a60d39266fd
Jose Daniel Rodriguez Sanchez <jdrs2483@gmail.com>
2021-09-15 15:48:37 -0600

    Build Systems - Module 8 - Final project

--- 1AngularVersion/tienda/src/app/app.component.ts
+++ 1AngularVersion/tienda/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tienda';
+  number = 0;
+
+  login_validation(email: string,password: string){
+
+
+    const user = email;
+    const pass = password;
+
+    console.log("This is a test: " + this.number)
+    this.number = this.number + 1;
+
+    if (user == "") {
+      console.log("User is empty");
+      alert("User field is empty!");
+    }
+    else {
+      if (pass == "") {
+        console.log("Password is empty");
+        alert("Password field is empty!");
+      }
+    }
+
+    
+
+
+  }
+
 }
